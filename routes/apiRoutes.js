c35e2e93c893deac899f9d5c7b4cddf28c55d2b4
benjaminbtrance <benjaminb.tran@gmail.com>
2021-07-22 18:38:54 -0500

    add api post to post to the db.json file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,7 +10,21 @@ module.exports = (app) => {
 	});
 
 	// API POST Requests
-	app.post('/api/notes', (req, res) => {});
+	app.post('/api/notes', (req, res) => {
+		fs.readFile('./db/db.json', 'utf8', (err, data) => {
+			const notes = JSON.parse(data);
+			notes.push(req.body);
+			fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+				if (err) {
+					console.log(err);
+					res.json(false);
+				} else {
+					console.log('Thank you for your notes.');
+					res.json(true);
+				}
+			});
+		});
+	});
 
 	// API DELETE Requests
 	app.delete('/api/notes/:id', (req, res) => {});
