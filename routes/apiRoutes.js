86fb612a9fd6583edb2760fffff287e502a99529
benjaminbtrance <benjaminb.tran@gmail.com>
2021-07-22 18:17:40 -0500

    add api get to get all notes back

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,10 +4,14 @@ const fs = require('fs');
 module.exports = (app) => {
 	// API GET Requests
 	app.get('/api/notes', (req, res) => {
-		fs.readFile('./db/db.json', (err, data) => {
-			if (err) throw err;
-			dbData = JSON.parse(data);
-			res.send(dbData);
+		fs.readFile('./db/db.json', 'utf8', (err, data) => {
+			res.json(JSON.parse(data));
 		});
 	});
+
+	// API POST Requests
+	app.post('/api/notes', (req, res) => {});
+
+	// API DELETE Requests
+	app.delete('/api/notes/:id', (req, res) => {});
 };
